9e19c9c450a6952ea6d76c6351e04d321992364a
Maxwell Lang <maxwlang@gmail.com>
2020-08-30 00:02:22 -0500

    Footer example, header example

---
+++ src/assets/js/footer-example.js
@@ -0,0 +1,4 @@
+// This code is exported as a footer js file.
+$(() => {
+    console.log('Hello from footer-example');
+});
---
+++ src/assets/js/header-example.js
@@ -0,0 +1,4 @@
+// This code is exported as a header js file.
+$(() => {
+    console.log('Hello from header-example');
+});
